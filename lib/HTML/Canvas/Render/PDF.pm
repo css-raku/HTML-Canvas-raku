64f7c6bbd2560a558cdac775734d3020025763a4
David Warring <david.warring@gmail.com>
2016-11-05 19:35:59 +1300

    correct !coords method. add ops beginPath, stroke and arc (stubbed)

--- lib/HTML/Canvas/Render/PDF.pm
+++ lib/HTML/Canvas/Render/PDF.pm
@@ -6,6 +6,7 @@ class HTML::Canvas::Render::PDF {
     has PDF::Content $.gfx handles <content> is required;
     has $.height is required; # canvas height in points
     has $.font-object is required;
+    has @!ctm = [1, 0, 0, 1, 0, 0]; #| canvas transform matrix
 
     method callback {
         sub ($op, |c) {
@@ -23,21 +24,53 @@ class HTML::Canvas::Render::PDF {
     sub pt(Numeric \l) { l }
 
     method !coords(Numeric \x, Numeric \y) {
-        #| translate back to absolute cordinates
-        my \ctm = $!gfx.CTM;
-        my (\x1, \y1) = PDF::Content::Util::TransformMatrix::dot(ctm, x, y);
-        PDF::Content::Util::TransformMatrix::inverse-dot(ctm, x1, $!height - y1);
+        #| translate back to canvas coordinates
+        my (\x1, \y1) = PDF::Content::Util::TransformMatrix::dot(@!ctm, x, y);
+        PDF::Content::Util::TransformMatrix::inverse-dot(@!ctm, x1, $!height - y1);
+    }
+
+    # ref: http://stackoverflow.com/questions/1960786/how-do-you-draw-filled-and-unfilled-circles-with-pdf-primitives
+    sub draw-circle(\g, Numeric \r) {
+        my Numeric \magic = r * 0.551784;
+        g.MoveTo(-r, 0);
+        g.CurveTo(-r, magic, -magic, r,  0, r);
+        g.CurveTo(magic, r,  r, magic,  r, 0);
+        g.CurveTo(r, -magic,  magic, -r,  0, -r);
+        g.CurveTo(-magic, -r,  -r, -magic,  -r, 0);
+    }
+
+    method !transform( |c ) {
+	my Numeric @tm = PDF::Content::Util::TransformMatrix::transform-matrix( |c );
+        @!ctm = PDF::Content::Util::TransformMatrix::multiply(@!ctm, @tm);
+	$!gfx.ConcatMatrix( @tm );
     }
 
     my %Dispatch = BEGIN %(
-        scale     => method (Numeric \x, Numeric \y) { $!gfx.transform(|scale => [x, y]) },
-        rotate    => method (Numeric \angle) { $!gfx.transform(|rotate => [ angle, ]) },
-        translate => method (Numeric \x, Numeric \y) { $!gfx.transform(|translate => [x, -y]) },
+        scale     => method (Numeric \x, Numeric \y) { self!transform(|scale => [x, y]) },
+        rotate    => method (Numeric \angle) { self!transform(|rotate => [ angle, ]) },
+        translate => method (Numeric \x, Numeric \y) { self!transform(|translate => [x, -y]) },
         transform => method (Numeric \a, Numeric \b, Numeric \c, Numeric \d, Numeric \e, Numeric \f) {
-            $!gfx.ConcatMatrix(a, b, c, d, e, -f);
+            self!transform(|matrix => [a, b, c, d, e, -f]);
         },
         setTransform => method (Numeric \a, Numeric \b, Numeric \c, Numeric \d, Numeric \e, Numeric \f) {
-            $!gfx.CTM = [a, b, c, d, e, -f];
+            my @ctm-inv = PDF::Content::Util::TransformMatrix::inverse(@!ctm);
+            my @diff = PDF::Content::Util::TransformMatrix::multiply([a, b, c, d, e, -f], @ctm-inv);
+                self!transform( |matrix => @diff )
+                    unless PDF::Content::Util::TransformMatrix::is-identity(@diff);
+        },
+        arc => method (Numeric \x, Numeric \y, Numeric \r, Numeric \startAngle, Numeric \endAngle, Bool $anti-clockwise?) {
+            # stub. ignores start and end angle; draws a circle
+            warn "todo: arc start/end angles"
+                unless endAngle - startAngle =~= 2 * pi;
+            $!gfx.ConcatMatrix:  PDF::Content::Util::TransformMatrix::translate(|self!coords(x, y) );
+            draw-circle($!gfx, r);
+        },
+        beginPath => method () {
+            $!gfx.Save;
+        },
+        stroke => method () {
+            $!gfx.Stroke;
+            $!gfx.Restore;
         },
         fillText => method (Str $text, Numeric $x, Numeric $y, Numeric $maxWidth?) {
             self.font;
